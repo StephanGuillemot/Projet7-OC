6841f2dfbca747c68c544b5daf8faf8e0fafc413
StephanGuillemot <stephanvtt@gmail.com>
2023-01-17 21:14:26 +0100

    Changement layout dashboard

--- dashboard.py
+++ dashboard.py
@@ -43,103 +43,105 @@ def main():
     
     index = st.selectbox('Quel client souhaitez vous consulter ?', data_test['SK_ID_CURR'].iloc[:100].to_list())
     #index = st.selectbox('Quel client souhaitez vous consulter ?', [387758])
-    col1, col2 = st.columns(2)
-    with col1:
-        show_btn = st.button('Afficher Information du client')
+    
+    #col1, col2 = st.columns(2)
+    with st.container():
+       # show_btn = st.button('Afficher Information du client')
 
-        if show_btn:
+       # if show_btn:
 
-            data = index
-            Affichage = None
+        data = index
+       # Affichage = None
 
-            Affichage = request_prediction(FastAPI_URI_show,  data)
-            AgGrid(pd.DataFrame(ast.literal_eval(Affichage)))
-            #st.write(f'{Affichage}')
+        Affichage = request_prediction(FastAPI_URI_show,  data)
+        AgGrid(pd.DataFrame(ast.literal_eval(Affichage)))
+        #st.write(f'{Affichage}')
         
         
     
     
-    with col2:    
-        predict_btn = st.button('Prédire')
-        if predict_btn:
-            data = index
-            pred = request_prediction(FastAPI_URI_pred, data)
+    with st.container():    
+      #  predict_btn = st.button('Prédire')
+       # if predict_btn:
+        data = index
+        pred = request_prediction(FastAPI_URI_pred, data)
 
 
 
-            option = {
-                "tooltip": {
-                    "formatter": '{a} <br/>{b} : {c}%'
+        option = {
+            "tooltip": {
+                "formatter": '{a} <br/>{b} : {c}%'
+            },
+            "series": [{
+                "name": 'Proba',
+                "type": 'gauge',
+                "axisLine": {
+                    "lineStyle": {
+                        "width": 5,
+                        "color": [
+                                [0.3, '#2FEB00'], # seuils intermédiaires
+                                [0.7, '#FF8114'],
+                                [1, '#FF0000']
+                            ]
+                        },},
+                "min" : 0,
+                "max" : 100,
+                "startAngle": 180,
+                "endAngle": 0,
+                "progress": {
+                    "show": "true"
                 },
-                "series": [{
-                    "name": 'Proba',
-                    "type": 'gauge',
-                    "axisLine": {
-                        "lineStyle": {
-                            "width": 5,
-                            "color": [
-                                    [0.3, '#2FEB00'], # seuils intermédiaires
-                                    [0.7, '#FF8114'],
-                                    [1, '#FF0000']
-                                ]
-                            },},
-                    "min" : 0,
-                    "max" : 100,
-                    "startAngle": 180,
-                    "endAngle": 0,
-                    "progress": {
-                        "show": "true"
-                    },
-                    "radius":'100%', 
+                "radius":'100%', 
 
-                    "itemStyle": {
-                        "color": '#58D9F9',
-                        "shadowColor": 'rgba(0,138,255,0.45)',
-                        "shadowBlur": 10,
-                        "shadowOffsetX": 2,
-                        "shadowOffsetY": 2,
-                        "radius": '55%',
-                    },
-                    "progress": {
-                        "show": "true",
-                        "roundCap": "true",
-                        "width": 6
-                    },
-                    "pointer": {
-                        "length": '60%',
-                        "width": 8,
-                        "offsetCenter": [0, '5%']
-                    },
-                    "detail": {
-                        "valueAnimation": "true",
-                        "formatter": '{value}',
-                        "backgroundColor": '#58D9F9',
-                        "borderColor": '#999',
-                        "borderWidth": 4,
-                        "width": '60%',
-                        "lineHeight": 20,
-                        "height": 20,
-                        "borderRadius": 188,
-                        "offsetCenter": [0, '40%'],
-                        "valueAnimation": "true",
-                    },
-                    "data": [{
-                        "value": round(float(pred['prediction']) * 100, 1) ,
-                        #"value": 40,
-                        "name": 'Prediction'
-                    }]
+                "itemStyle": {
+                    "color": '#58D9F9',
+                    "shadowColor": 'rgba(0,138,255,0.45)',
+                    "shadowBlur": 10,
+                    "shadowOffsetX": 2,
+                    "shadowOffsetY": 2,
+                    "radius": '55%',
+                },
+                "progress": {
+                    "show": "true",
+                    "roundCap": "true",
+                    "width": 6
+                },
+                "pointer": {
+                    "length": '60%',
+                    "width": 8,
+                    "offsetCenter": [0, '5%']
+                },
+                "detail": {
+                    "valueAnimation": "true",
+                    "formatter": '{value}',
+                    "backgroundColor": '#58D9F9',
+                    "borderColor": '#999',
+                    "borderWidth": 4,
+                    "width": '60%',
+                    "lineHeight": 20,
+                    "height": 20,
+                    "borderRadius": 188,
+                    "offsetCenter": [0, '40%'],
+                    "valueAnimation": "true",
+                },
+                "data": [{
+                    "value": round(float(pred['prediction']) * 100, 1) ,
+                    #"value": 40,
+                    "name": 'Prediction'
                 }]
-            };
+            }]
+        };
 
 
-            st_echarts(options=option, key="1") 
+        st_echarts(options=option, key="1") 
 
         
     
 
     
-    explain_btn = st.button('Expliquer')
-    if explain_btn:
+    #explain_btn = st.button('Expliquer')
+   # if explain_btn:
+    with st.container():
         data = index
 
         explain_plot = ast.literal_eval(request_prediction(FastAPI_URI_explain, data))
