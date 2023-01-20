6ce48d9ad44950be0edf225452a95e49794c1fce
StephanGuillemot <stephanvtt@gmail.com>
2023-01-21 00:37:33 +0100

    Correction bug dashboard

--- dashboard.py
+++ dashboard.py
@@ -158,11 +158,11 @@ def main():
     with st.container() :
         Colonne = st.selectbox('Quel variable souhaitait vous consulter ? ', data_test.drop(['SK_ID_CURR'], axis = 1).columns.values)
     with st.container():
-        fig1 = sns.kdeplot(data = data_test, x = Colonne, hue = 'TARGET', common_norm = True)
-        fig1.plot([float(data_test[data_test['SK_ID_CURR'] == index][Colonne]), float(data_test[data_test['SK_ID_CURR'] == index][Colonne])] , [0,1], 'r', linestyle = 'dashed')
+        fig = sns.kdeplot(data = data_test, x = Colonne, hue = 'TARGET', common_norm = True)
+        fig.plot([float(data_test[data_test['SK_ID_CURR'] == index][Colonne]), float(data_test[data_test['SK_ID_CURR'] == index][Colonne])] , [0,1], 'r', linestyle = 'dashed')
         plt.xlabel(f'Valeur {Colonne}')
         plt.ylabel(f'Répartition {Colonne} Defaut et Sains')
-        st.pyplot(fig=fig1)
+        st.pyplot(fig=fig)
         
         
         
